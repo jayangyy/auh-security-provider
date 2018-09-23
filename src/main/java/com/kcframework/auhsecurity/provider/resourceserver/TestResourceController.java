0f4abd5de182f87441ea5ebb8f7200714a4f5ae4
yang <361300115@qq.com>
2018-09-23 08:24:25 +0800

    hebing

---
+++ src/main/java/com/kcframework/auhsecurity/provider/resourceserver/TestResourceController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kcframework.auhsecurity.provider.resourceserver;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author jayan
+ */
+@RestController
+@RequestMapping("/testRes")
+public class TestResourceController {
+
+    @RequestMapping("/sayHello/{name}")
+    public String HellowController(@RequestParam("name") String name) {
+        return "Hello this is " + name;
+    }
+
+}
